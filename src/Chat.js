c0ce65db8ac2a16357d60c9eb0b1b1b79a2a40f0
Tammibriggs <Godsfirstbriggs@gmail.com>
2021-10-17 06:23:26 +0100

    built UI

---
+++ src/Chat.js
@@ -0,0 +1,70 @@
+import './chat.css'
+import { Avatar, IconButton } from '@mui/material';
+import {useState, useEffect} from 'react'
+import {SearchOutlined, AttachFile, MoreVert} from '@mui/icons-material';
+import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon';
+import MicIcon from '@mui/icons-material/Mic';
+
+function Chat() {
+
+  const [seed, setSeed] = useState('')
+  const [input, setInput] = useState('')
+
+  useEffect(() => {
+    setSeed(Math.floor(Math.random() * 500))
+  }, [])
+
+  const sendMessage = e => {
+    e.preventDefault()
+    console.log('yoo types >> ', input)
+    setInput('')
+  }
+
+  return (
+    <div className='chat'>
+      <div className='chat__header'>
+        <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`}/>
+       
+        <div className='chat__headerInfo'>
+          <h3>Room name</h3>
+          <p>Last seen at ...</p>
+        </div>
+        <div className='chat__headerRight'>
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton >
+            <AttachFile />
+          </IconButton >
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+
+      <div className='chat__body'>
+        <p className= {`chat__message ${true && 'chat__reciever'}`}>
+          <span className='chat__name'>
+            Tammibriggs
+          </span>
+          Hey Guys
+          <span className='chat__timestamp'>3.52pm</span>
+        </p>
+        <p className='chat__message'>Hey Guys</p>
+        <p className='chat__message'>Hey Guys</p>
+      </div>
+      <div className='chat__footer'>
+        <InsertEmoticonIcon />
+        <form>
+          <input type='text' placeholder='Type a message' value={input} onChange={e => setInput(e.target.value) }/>
+          <button
+            onClick={sendMessage}
+            type='submit'>Send a message</button>
+        </form>
+        <MicIcon />
+      </div>
+    </div>
+  )
+}
+
+export default Chat
---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app"
+import { getAuth, GoogleAuthProvider } from "firebase/auth"
+import { getFirestore } from "firebase/firestore";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDBExCaGI2cqJCdBWGqQJ0Vpua1yFiw9R4",
+  authDomain: "whatsapp-clone-3435d.firebaseapp.com",
+  projectId: "whatsapp-clone-3435d",
+  storageBucket: "whatsapp-clone-3435d.appspot.com",
+  messagingSenderId: "895019269278",
+  appId: "1:895019269278:web:3f53f7abc5790f1436cf8f"
+};
+
+const app = initializeApp(firebaseConfig)
+const auth = getAuth(app)
+const db = getFirestore(app)
+const provider = new GoogleAuthProvider(auth)
+
+export default db
+export {auth, provider}
